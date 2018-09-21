460cc21dbd0a0af11ff4d2ab8f65487fb4c0c3bf
Vera Klimina <veraklim>
2018-09-21 16:51:55 +0300

    add new var a and b

--- task1.scala
+++ task1.scala
@@ -1,14 +1,4 @@
 
-
 class task1 {
-  var a = 6
-  var b = 7
-<<<<<<< HEAD
-  var sum = a + b
-  var diff = b - a
-  var mult = a*b
-=======
-  var sum = a + b
-  var diff = b - a
->>>>>>> change
+
 }
